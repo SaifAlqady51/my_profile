4755eb81b7469d50394f1b1b460be342ab151644
SaifAlqady51 <saifalqady51@gmail.com>
2024-02-10 19:11:02 +0200

    Put Github icon in layout page

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,10 +1,28 @@
 <script>
 	import './styles.css';
 	import Navbar from '$lib/navbar/Navbar.svelte';
+
+	import Icon from 'svelte-icons-pack/Icon.svelte';
+	import FaBrandsGithubAlt from 'svelte-icons-pack/fa/FaBrandsGithubAlt';
 </script>
 
 <div id="app" class="font-press-start">
 	<Navbar />
+
+	<!-- Github Link -->
+	<a
+		href="https://github.com/SaifAlqady51/"
+		target="_blank"
+		class="absolute bottom-10 left-10 hover:bg-[#415a77] rounded-full p-5"
+	>
+		<Icon
+			src={FaBrandsGithubAlt}
+			color="white"
+			size="64"
+			className="custom-icon"
+			title="Custom icon params"
+		/>
+	</a>
 </div>
 
 <slot />
