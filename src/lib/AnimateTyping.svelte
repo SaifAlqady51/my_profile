6d1fcf028fe42021cf85f575e9a0200fb1f79155
SaifAlqady51 <saifalqady51@gmail.com>
2024-02-14 11:40:10 +0200

    Created AnimateTyping Component

---
+++ src/lib/AnimateTyping.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+	import Icon from 'svelte-icons-pack/Icon.svelte';
+	import FaWindowMinimize from 'svelte-icons-pack/fa/FaWindowMinimize';
+
+	export let sentence: string;
+	let typedChar = '';
+
+	const addCharTotypedChar = (char: string) => {
+		typedChar += char;
+		return typedChar;
+	};
+
+	const addChartoStrigAfterOneSec = (sentence: string, i: number) => {
+		setTimeout(() => {
+			return addCharTotypedChar(sentence[i]);
+		}, i * 80);
+	};
+
+	export const animateTyping = (sentence: string) => {
+		for (let i = 0; i < sentence.length; i++) {
+			addChartoStrigAfterOneSec(sentence, i);
+		}
+	};
+
+	animateTyping(sentence);
+</script>
+
+<div class="">
+	<h1 class="h-fit leading-10 md:leading-[80px]">{typedChar}</h1>
+</div>
