319ba391f741ecefd7d87f15bc43d326ab0b0de5
SaifAlqady51 <saifalqady51@gmail.com>
2024-02-14 11:41:19 +0200

    Deleted Animate function

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,35 +1,10 @@
 <script lang="ts">
-	let greetingSentence = 'Hello, My name is Saif';
-	let typedChar = '';
-
-	const addCharTotypedChar = (char: string) => {
-		typedChar += char;
-		return typedChar;
-	};
-
-	const addChartoStrigAfterOneSec = (sentence: string, i: number) => {
-		setTimeout(() => {
-			return addCharTotypedChar(sentence[i]);
-		}, i * 120);
-	};
-
-	export const animateTyping = (sentence: string) => {
-		for (let i = 0; i < sentence.length; i++) {
-			addChartoStrigAfterOneSec(sentence, i);
-		}
-	};
-
-	animateTyping(greetingSentence);
+	import AnimateTyping from '$lib/AnimateTyping.svelte';
 </script>
 
 <div class="w-screen h-screen flex justify-center items-center">
 	<!-- <div class="flex w-fit h-full z-40 justify-between gap-20"> -->
-	<div>
-		<h1
-			id="name"
-			class="  md:text-5xl text-xl text-white font-semibold h-16 flex items-center px-4 overflow-auto font-press-start"
-		>
-			{typedChar}
-		</h1>
+	<div class="text-white text-5xl font-press-start">
+		<AnimateTyping sentence="Hello, My name is Saif" />
 	</div>
 </div>
--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,30 +1,11 @@
 <script lang="ts">
-	let introductoin =
-		"I'm a Front-End developer from Egypt, I like to convert ideas into products. Interested in clean code principles, Mostly use Next.js, Typescript and Tailwind,   ";
-	let typedChar = '';
-
-	const addCharTotypedChar = (char: string) => {
-		typedChar += char;
-		return typedChar;
-	};
-
-	const addChartoStrigAfterOneSec = (sentence: string, i: number) => {
-		setTimeout(() => {
-			return addCharTotypedChar(sentence[i]);
-		}, i * 80);
-	};
-
-	export const animateTyping = (sentence: string) => {
-		for (let i = 0; i < sentence.length; i++) {
-			addChartoStrigAfterOneSec(sentence, i);
-		}
-	};
-
-	animateTyping(introductoin);
+	import AnimateTyping from '$lib/AnimateTyping.svelte';
 </script>
 
 <div
 	class="w-screen h-screen md:text-4xl text-xl font-press-start text-white flex justify-start mt-40 px-5 md:px-[18%]"
 >
-	<h1 class="h-fit leading-10 md:leading-[80px]">{typedChar}</h1>
+	<AnimateTyping
+		sentence="I'm a Front-End developer from Egypt, I like to convert ideas into products. Interested in clean code principles, Mostly use Next.js, Typescript and Tailwind"
+	/>
 </div>
