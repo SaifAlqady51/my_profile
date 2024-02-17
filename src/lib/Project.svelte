9284197df734d608b271991d4d45384c5868c19f
SaifAlqady51 <saifalqady51@gmail.com>
2024-02-17 14:49:02 +0200

    Created Project component

---
+++ src/lib/Project.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+	export let projectName: string;
+	export let githubLink: string;
+	export let websiteLink: string;
+
+	import Icon from 'svelte-icons-pack';
+	import AiFillGithub from 'svelte-icons-pack/ai/AiFillGithub';
+	import CgWebsite from 'svelte-icons-pack/cg/CgWebsite';
+</script>
+
+<div class="w-full h-fit flex justify-between">
+	<p>{projectName}</p>
+	<div class="flex gap-16">
+		<a href={websiteLink} target="_blank">
+			<Icon
+				src={CgWebsite}
+				color="white"
+				size="64"
+				className="custom-icon"
+				title="Custom icon params"
+			/>
+		</a>
+		<a href={githubLink} target="_blank">
+			<Icon
+				src={AiFillGithub}
+				color="white"
+				size="64"
+				className="custom-icon"
+				title="Custom icon params"
+			/>
+		</a>
+	</div>
+</div>
