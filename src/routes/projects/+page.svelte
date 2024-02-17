ad40369387b1ed3e9f220c716dec208a21e556b8
SaifAlqady51 <saifalqady51@gmail.com>
2024-02-17 14:50:02 +0200

    Created project page

---
+++ src/routes/projects/+page.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	import Project from '$lib/Project.svelte';
+</script>
+
+<div
+	class="w-screen h-screen md:text-4xl text-xl font-press-start text-white flex flex-col justify-start mt-40 px-20 gap-20"
+>
+	<Project
+		projectName="Flight booking website (Tayara)"
+		githubLink="https://github.com/SaifAlqady51/flight_booking_website"
+		websiteLink="https://flight-booking-website-git-main-saifalqady51.vercel.app/"
+	/>
+	<Project
+		projectName="Airports API gui website (Firnas)"
+		githubLink="https://github.com/SaifAlqady51/firnas"
+		websiteLink="https://firnas-weld.vercel.app/"
+	/>
+	<Project
+		projectName="Airport API "
+		githubLink="https://github.com/saifAlqady51/airport-api"
+		websiteLink="https://airport-api-gamma.vercel.app/api/airport/"
+	/>
+</div>
