61a272b041989e75353bc4a52b7285705fe6037c
SaifAlqady51 <saifalqady51@gmail.com>
2024-03-02 15:13:08 +0200

    ui: created email template

--- src/lib/emails/Hello.svelte
+++ src/lib/emails/Hello.svelte
@@ -2,12 +2,21 @@
 	import { Button, Hr, Html, Text } from 'svelte-email';
 
 	export let name = 'World';
+	export let email = 'unkown';
+	export let body = '';
 </script>
 
 <Html lang="en">
 	<Text>
+		From: {email}
+	</Text>
+	<Text>
 		Hello, {name}!
 	</Text>
+	<Text>
+		{body}
+	</Text>
+
 	<Hr />
 	<Button href="https://svelte.dev">Visit Svelte</Button>
 </Html>
