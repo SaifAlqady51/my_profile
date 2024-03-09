012506c92ecf230daa26f3e1dd5cc0d5c35f701c
SaifAlqady51 <saifalqady51@gmail.com>
2024-03-09 18:32:20 +0200

    styles: Made work page responsive

--- src/routes/work/+page.svelte
+++ src/routes/work/+page.svelte
@@ -1,18 +1,27 @@
 <script lang="ts">
 </script>
 
-<div class="h-screen w-screen flex flex-col items-center mt-40 font-press-start">
+<div class="h-screen w-screen flex flex-col items-center md:mt-40 mt-32 font-press-start">
 	<div class="flex flex-col w-fit items-center">
-		<div class="flex gap-10 items-center h-fit text-2xl text-white">
-			<h4 class="w-80">December 2023</h4>
-			<div class="w-20 h-20 border-[3px] border-black rounded-full bg-[#ED1E79]"></div>
-			<a href="https://evvent.no/" target="_blank" class="cursor-pointer w-80"> Evvent AS </a>
-		</div>
-		<div class="w-1 h-96 bg-white my-2"></div>
+		<div class="flex gap-10 items-center h-fit md:text-2xl text-sm text-white">
+			<h4 class="md:w-80 w-20">December 2023</h4>
+			<!-- circle -->
+			<div
+				class="md:w-20 md:h-20 w-10 h-10 border-[3px] border-black rounded-full bg-[#ED1E79]"
+			></div>
 
-		<div class="flex gap-10 items-center h-fit w-full text-2xl text-white justify-start">
-			<h4 class="w-80 flex justify-end">Present</h4>
-			<div class="w-20 h-20 border-[3px] border-black rounded-full bg-[#ED1E79]"></div>
+			<a href="https://evvent.no/" target="_blank" class="cursor-pointer md:w-80 w-20">
+				Evvent AS
+			</a>
+		</div>
+		<!-- cane -->
+		<div class="w-1 md:h-96 h-60 bg-white my-2"></div>
+
+		<div class="flex gap-10 items-center h-fit w-full md:text-2xl text-sm text-white justify-start">
+			<h4 class="md:w-80 w-20 flex justify-end">Present</h4>
+			<div
+				class="md:w-20 md:h-20 w-10 h-10 border-[3px] border-black rounded-full bg-[#ED1E79]"
+			></div>
 		</div>
 	</div>
 </div>
